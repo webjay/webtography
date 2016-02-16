eafa067ee9fb4660d87c4d90eece9d3a708bbc1d
Jacob Friis Saxberg <jacob@saxberg.dk>
2016-02-16 10:13:25 +0100

    wget via Node

--- worker.js
+++ worker.js
@@ -1,3 +1,28 @@
 'use strict';
 
-console.log('hello');
+const execFile = require('child_process').execFile;
+const os = require('os');
+
+const options = {
+  cwd: os.tmpdir(),
+  timeout: 30 * 1000,
+  maxBuffer: 10 * 1024
+};
+
+const args = [
+  '--no-cookies', 
+  '--timestamping',
+  // '--quiet', 
+  '--recursive',
+  '--level=10', 
+  '--convert-links', 
+  '--no-parent'
+];
+
+execFile('wget', args.concat(['http://www.webcom.dk/']), options, (err, stdout, stderr) => {
+  if (err !== null) {
+    throw err;
+  }
+	console.log(`stdout: ${stdout}`);
+	console.log(`stderr: ${stderr}`);
+});
