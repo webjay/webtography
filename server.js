dde57538699af523b11ed381c75575156eaffd1c
Jacob Friis Saxberg <jacob@saxberg.dk>
2016-02-24 09:34:24 +0100

    status codes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const createServer = require('http').createServer;
-const qs = require('querystring');
+const qsParse = require('querystring').parse;
 const worker = require('./worker.js');
 
 const Port = process.env.PORT || 3000;
@@ -14,30 +14,35 @@ function handler (request, response) {
   response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST');
   response.setHeader('Access-Control-Allow-Headers', 'accept, origin, content-type');
-  if (request.method === 'OPTIONS') {
-    return response.end();
-  }
-  if (request.method === 'GET') {
-    return defaultResponse(response);
-  }
-  if (request.method === 'POST') {
-    var body = '';
-    request.on('data', (data) => {
-      body += data;
-      // keep it below 1mb
-      if (body.length > 1e6) {
-        request.connection.destroy();
-      }
-    });
-    request.on('end', () => {
-      const post = qs.parse(body);
-      if (!post.url || !post.username || !post.token) {
-        return defaultResponse(response);
-      }
-      response.end('I got this: ' + post.url);
-      console.log('Working with', post.url, post.username, post.token.substr(-5));
-      worker(post.url, post.username, post.token);
-    });
+  switch (request.method) {
+    case 'OPTIONS':
+      response.end();
+      break;
+    case 'POST':
+      var body = '';
+      request.on('data', (data) => {
+        body += data;
+        // keep it below 1mb
+        if (body.length > 1e6) {
+          response.writeHead(413);
+          request.connection.destroy();
+        }
+      });
+      request.on('end', () => {
+        const post = qsParse(body);
+        if (!post.url || !post.username || !post.token) {
+          response.writeHead(409);
+          return defaultResponse(response);
+        }
+        response.writeHead(201);
+        response.end('I got this: ' + post.url);
+        console.log('Working with', post.url, post.username, '...' + post.token.substr(-5));
+        worker(post.url, post.username, post.token);
+      });
+      break;
+    default:
+      defaultResponse(response);
+      break;
   }
 }
 
