d92d9cf761da5683c8163c2f2f70aee94d8d262b
Jacob Friis Saxberg <jacob@saxberg.dk>
2016-02-24 08:54:37 +0100

    CORS fix

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ const worker = require('./worker.js');
 
 const Port = process.env.PORT || 3000;
 
+function defaultResponse (response) {
+  response.end('Please POST me a url, username and token');
+}
+
 function handler (request, response) {
   response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Allow-Methods', 'POST');
+  response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST');
   if (request.method === 'POST') {
     var body = '';
     request.on('data', (data) => {
@@ -20,11 +24,14 @@ function handler (request, response) {
     });
     request.on('end', () => {
       const post = qs.parse(body);
+      if (!post.url || !post.username || !post.token) {
+        return defaultResponse(response);
+      }
       response.end('I got this: ' + post.url);
       worker(post.url, post.username, post.token);
     });
   } else {
-    response.end('Please POST me a url, username and token');
+    defaultResponse(response);
   }
 }
 
